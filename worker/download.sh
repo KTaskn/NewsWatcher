c1cffca2554c88e17b137a573982a1c10f8dc050
KTaskn <korai.kinjo@gmail.com>
2020-06-14 20:24:55 +0900

    init

---
+++ worker/download.sh
@@ -0,0 +1,10 @@
+download_date=$1
+shinbun=$2
+url=$3
+
+name=${download_date}_${shinbun}
+savedir=${DIRNAME_ARCHIVES}/${name}
+mkdir savedir
+
+echo ${savedir}
+wget -P ${savedir} -A .html ${url}
---
+++ worker/run.sh
@@ -0,0 +1,9 @@
+
+download_date=`date +%Y%m%d%H%M%S`
+
+while read row; do
+    echo $row
+    shinbun=`echo ${row} | cut -d , -f 1`
+    url=`echo ${row} | cut -d , -f 2`
+    /work/download.sh $download_date $shinbun $url
+done < urls.csv
